57238ae3e3b4a3d723a2a7d461868ddcb9abe2cc
Sang Nguyen <ngcongsang@gmail.com>
2021-09-14 00:16:17 +0700

    final commit

--- tests/test_web.py
+++ tests/test_web.py
@@ -10,24 +10,18 @@ from pages.search import SearchPage
 def browser():
     # Initialize ChromeDriver
     driver = webdriver.Chrome(ChromeDriverManager().install())
-    # Wait implicitly for elements to be ready before attempting interactions
     driver.implicitly_wait(10)
 
-    # Return the driver object at the end of setup
     yield driver
 
-    # For cleanup, quit the driver
     driver.quit()
 
 
 def test_basic_duckduckgo_search(browser):
-    # Set up test case data
     PHRASE = 'panda'
-    # Search for the phrase
     search_page = SearchPage(browser)
     search_page.load()
     search_page.search(PHRASE)
-    # Verify that results appear
     result_page = ResultPage(browser)
     assert result_page.link_div_count() > 0
     assert result_page.phrase_result_count(PHRASE) > 0
